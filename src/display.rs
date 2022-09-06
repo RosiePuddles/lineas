204ae88b90c9323e5d3f78427cd8e23513f79080
nxe <rosiegbartlett@gmail.com>
2022-09-06 12:04:33 +0100

    finally, a display impl

--- src/display.rs
+++ src/display.rs
@@ -1,11 +1,35 @@
 use std::fmt::{Formatter, Display, Debug};
-use crate::{Matrix, Vector, polynomials::Polynomial};
-use conv::{ConvUtil, ValueFrom, ValueInto};
+use crate::{Matrix, polynomials::Polynomial};
+use conv::{ConvUtil, ValueFrom};
 use itertools::Itertools;
 
 impl<const T: usize, const N: usize, L: Copy + Debug + Display> Display for Matrix<T, N, L> {
+	/// Display [`Matrix`], [`Vector`], or [`ColVector`]
+	///
+	/// You can specify precision, width, and the presence of a sign before the elements
+	///
+	/// For [`ColVectors`] (or more specifically any `Matrix<T, 1, L>`) you can also specify the
+	/// alternative display with the `#` flag. This will return the vector all on one line with a
+	/// superscript T at the end to indicate a transposition.
 	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-		todo!()
+		fn fm<L>(q: &L, f: &&mut Formatter<'_>) -> String where L: Display {
+			let p = f.precision().unwrap_or(0);
+			let w = f.width().unwrap_or(0);
+			if f.sign_plus() {
+				format!("{:+w$.p$}", q)
+			} else {
+				format!("{:w$.p$}", q)
+			}
+		}
+		if T == 0 || N == 0 {
+			write!(f, "")
+		} else if N == 1 && f.alternate() {
+			write!(f, "|{}|ᵀ", self.0.iter().map(|t| fm(&t[0], &f)).collect::<Vec<String>>().join(", "))
+		} else {
+			write!(f, "{}", self.0.iter().map(|t| {
+				format!("|{}|", t.iter().map(|l| fm(l, &f)).collect::<Vec<String>>().join(", "))
+			}).collect::<Vec<String>>().join("\n"))
+		}
 	}
 }
 
